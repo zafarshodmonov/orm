ed773cea474a4b5337b2036babdf769cf9be1c0a
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2025-03-16 12:32:12 +0500

    z

---
+++ main.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+
+# SQLite ma'lumotlar bazasiga bog'lanish
+engine = create_engine('sqlite:///example.db', echo=True)
+
+# Ma'lumotlar bazasini yaratish
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'  # SQL jadval nomi
+	
+    id = Column(Integer, primary_key=True)     
+    name = Column(String, nullable=False)     
+    age = Column(Integer)         
+
+
+# Ma'lumotlar bazasiga bog'lanish
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# new_user = User(name="Ali", age=25)
+# session.add(new_user)
+# session.commit()  # O‘zgarishlarni bazaga saqlash
+# session.close()  # Sessionni yopish
+
+users = session.query(User).all()
+for user in users:
+    print(user.id, user.name, user.age)
+
+
